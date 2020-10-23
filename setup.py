bec660c1536825547a8607b767200d4fe128b2b6
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-23 13:03:13 -0700

    Initial commit for setup.py to use pysheetgrader properly

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pysheetgrader',
+    version='0.1',
+    py_modules=['pysheetgrader'],
+    install_requires=[
+        'openpyxl', 'sympy'
+    ]
+)
