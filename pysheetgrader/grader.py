670c2c4e1349ae9168547c1c4063b5a375f72b2e
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-22 22:21:45 -0700

    Initial commit for adding grade() method to Grader class

--- pysheetgrader/grader.py
+++ pysheetgrader/grader.py
@@ -5,9 +5,30 @@ class Grader:
     """
 
     def __init__(self, key_document):
+        """
+        Initializer of this instance.
+
+        :param key_document: Document instance of a valid key.
+        :exception ValueError: Raises a ValueError if the passed `key_document` is not a valid key.
+        """
         # Sanity check
         if not key_document or not key_document.is_valid_key():
             raise ValueError(f"The document passed is not a valid key. Path: {key_document.path}")
 
         # Attributes
         self.key_document = key_document
+        self.grading_sheet_names = key_document.grading_sheet_names()
+
+    def grade(self, document):
+        """
+        Grade the passed document against this instance's key document.
+        :param document: Document instance.
+        :return: Float value of the grade.
+        """
+
+        score = 0
+        for sheet_name in self.grading_sheet_names:
+            # TODO: Add proper implementation for this method.
+            score += 10
+
+        return score
