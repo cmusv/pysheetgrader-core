1dd362eabf48e659e7b2964ce46d2f38048e8bef
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-23 14:33:40 -0700

    Add better explanation for NaiveFormulaStrategy's exception

--- pysheetgrader/grading/strategy/formula.py
+++ pysheetgrader/grading/strategy/formula.py
@@ -14,12 +14,14 @@ class NaiveFormulaStrategy(BaseStrategy):
 
         cell_cord = self.grading_rubric.cell_cord
 
+        key_cell_value = key_sheet[cell_cord].value
+        sub_cell_value = sub_sheet[cell_cord].value
         try:
-            key_formula = parse_formula(key_sheet[cell_cord].value)
-            sub_formula = parse_formula(sub_sheet[cell_cord].value)
+            key_formula = parse_formula(key_cell_value)
+            sub_formula = parse_formula(sub_cell_value)
             is_similar = simplify(key_formula - sub_formula) == 0
             return self.grading_rubric.score if is_similar else 0
         except Exception as exc:
-            print(f"Failed to compare formulas. Error:")
+            print(f"Failed to compare formulas, key: {key_cell_value}, submission: {sub_cell_value}. Error:")
             print(exc)
             return 0
