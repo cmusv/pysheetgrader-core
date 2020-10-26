74ca1d8649312d821b27b2146ae6893e87f85633
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-26 12:05:02 -0700

    Add file path arguments to main.py

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import click
 
 
 @click.command()
-def cli():
-    print("Hello World! This is the PySheetGrader.")
+@click.argument('key_document',
+                type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
+@click.argument('submission_document',
+                type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
+def cli(key_document, submission_document):
+    """ Grades the passed SUBMISSION_DOCUMENT using the KEY_DOCUMENT."""
+    print("PySheetGrader!")
+    print(f"Input key document: {key_document}")
+    print(f"Submission key document: {submission_document}")
