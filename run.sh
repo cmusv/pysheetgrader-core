492fdf2c0b81a07a34912e869c88124fd8e454ee
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-30 16:02:56 -0700

    Update run.sh to use vocareum env variable

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
- #!/bin/sh
- # TODO: Add proper installation / environment check steps here.
- source venv/bin/activate
- pysheetgrader sample_excel_files/type_1_key.xlsx sample_excel_files/type_1_sub_right_different_formula_1.xlsx --score-output ./score-output.csv --report-output ./report-output.txt --verbose
+#!/bin/sh
+# TODO: Add proper installation / environment check steps here.
+source venv/bin/activate
+
+KEY_DOC_PATH=sample_excel_files/type_1_key.xlsx
+SUB_DOC_PATH=sample_excel_files/type_1_sub_right_different_formula_1.xlsx
+SCORE_OUTPUT_PATH=$vocareumGradeFile
+REPORT_OUTPUT_PATH=$vocareumReportFile
+pysheetgrader $KEY_DOC_PATH $SUB_DOC_PATH --score-output $SCORE_OUTPUT_PATH --report-output $REPORT_OUTPUT_PATH --verbose
