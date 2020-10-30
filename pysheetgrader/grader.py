666ad0cfe2299f889d9311d1017af508a04947ed
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-29 18:51:44 -0700

    Use Report instance in grader.py

--- pysheetgrader/grader.py
+++ pysheetgrader/grader.py
@@ -2,6 +2,7 @@ from pysheetgrader.grading.rubric import GradingRubric
 from pysheetgrader.grading.rubric import GradingRubricType
 from pysheetgrader.grading.strategy.constant import ConstantStrategy
 from pysheetgrader.grading.strategy.formula import NaiveFormulaStrategy
+from pysheetgrader.grading.report import Report
 
 
 class Grader:
@@ -28,27 +29,27 @@ class Grader:
         """
         Grade the passed `document` against this instance's key document.
         :param document: Document instance.
-        :return: Float value of the grade.
+        :return: Report instance of the grade.
         """
-        score = 0
+        report = Report()
         for sheet_name in self.grading_sheet_names:
-            score += self.grade_sheet(document, sheet_name)
+            report += self.grade_sheet(document, sheet_name)
 
-        return score
+        return report
 
     def grade_sheet(self, document, sheet_name):
         """
         Grade the passed `sheet_name` of the passed `document` against this instance's key document.
         :param document: Document instance.
         :param sheet_name: String value of the sheet name that should be graded.
-        :return: Float value of the grade for the sheet.
+        :return: Report instance of the grade for the sheet.
         """
-        score = 0
+        report = Report()
         rubrics = GradingRubric.create_rubrics_for_sheet(self.key_document, sheet_name)
         for r in rubrics:
-            score += self.grade_sheet_by_rubric(document, sheet_name, r)
+            report += self.grade_sheet_by_rubric(document, sheet_name, r)
 
-        return score
+        return report
 
     def grade_sheet_by_rubric(self, document, sheet_name, rubric):
         """
@@ -56,7 +57,7 @@ class Grader:
         :param document: Document instance.
         :param sheet_name: String value of the sheet name.
         :param rubric: GradingRubric instance.
-        :return: Float value of the grade of the document's sheet.
+        :return: Report instance of the grade of the document's sheet.
         """
         if rubric.rubric_type == GradingRubricType.CONSTANT:
             return ConstantStrategy(self.key_document, document, sheet_name, rubric).grade()
