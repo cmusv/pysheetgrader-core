7fad397f489bc91c244420237ba99e0eb3d2845b
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-10-29 18:44:35 -0700

    Add append method for report.py

--- pysheetgrader/grading/report.py
+++ pysheetgrader/grading/report.py
@@ -1,8 +1,23 @@
 
 class Report:
+    """
+    Representation of a grading report.
+    """
 
     def __init__(self):
         self.submission_score = 0
         self.max_possible_score = 0
-
         self.report_lines = []
+
+    def append(self, other_report):
+        """
+        Appends another Report instance properties to this instance.
+        :param other_report: Report instance.
+        """
+        # Early return
+        if not isinstance(other_report, self.__class__):
+            return
+
+        self.submission_score += other_report.submission_score
+        self.max_possible_score += other_report.max_possible_score
+        self.report_lines.extend(self.report_lines)
