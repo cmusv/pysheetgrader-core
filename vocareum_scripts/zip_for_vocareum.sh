17bcb4268c6523b35ff4bb6bd500871a52dc85d1
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-12-01 13:14:34 -0800

    Add script for zipping the whole folder for Vocareum

---
+++ vocareum_scripts/zip_for_vocareum.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script should be executed from PySheetGrader's root folder.
+
+zip -r pysheetgrader-vocareum.zip pysheetgrader vocareum_scripts requirements.txt setup.py setup.sh
