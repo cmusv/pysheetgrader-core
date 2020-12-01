6e0e443a4163f3dbbf0cbe36754432fd0af2bcc0
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-12-01 13:32:58 -0800

    Update zip_for_vocareum.sh

--- vocareum_scripts/zip_for_vocareum.sh
+++ vocareum_scripts/zip_for_vocareum.sh
@@ -2,4 +2,14 @@
 
 # This script should be executed from PySheetGrader's root folder.
 
-zip -r pysheetgrader-vocareum.zip pysheetgrader vocareum_scripts requirements.txt setup.py setup.sh
+# Copy necessary files to pysheetgrader-vocareum
+mkdir pysheetgrader-vocareum
+cp -r pysheetgrader pysheetgrader-vocareum
+cp -r vocareum_scripts pysheetgrader-vocareum
+cp ./{requirements.txt,setup.py,setup.sh} pysheetgrader-vocareum
+
+# Zip the folder
+zip -r pysheetgrader-vocareum.zip pysheetgrader-vocareum
+
+# Folder cleanup
+rm -rf pysheetgrader-vocareum
