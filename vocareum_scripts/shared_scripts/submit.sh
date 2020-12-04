5ab78a05ccdad7a103fe909648514e7d929f15fb
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-12-03 17:13:35 -0800

    Update submit.sh

--- vocareum_scripts/shared_scripts/submit.sh
+++ vocareum_scripts/shared_scripts/submit.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Local dependency setup
+# This is necessary because the submission workspace is different than the normal student workspace.
+# The path relies on the folder structure. Check zip_for_vocareum.sh as reference.
+
+source $LIB/pysheetgrader-vocareum/vocareum_scripts/student/setup_venv.sh > /dev/null
+
+# Activate python virtual environment
+
+source venv/bin/activate
 
 # Path setup
 
@@ -9,8 +18,7 @@ SUB_DOC_PATH=$HOME/submission.xlsx
 SCORE_OUTPUT_PATH=$vocareumGradeFile
 REPORT_OUTPUT_PATH=$vocareumReportFile
 
-# Activate env
-source $LIB/pysheetgrader-0.3/venv/bin/activate
-
 # Execute grading
+
 pysheetgrader $KEY_DOC_PATH $SUB_DOC_PATH --score-output $SCORE_OUTPUT_PATH --report-output $REPORT_OUTPUT_PATH --verbose
+
