8019a2d1d98a53ea8822a7f2d3478f0fc4f4e5c2
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-12-11 13:42:17 -0800

    Implement custom formula parsing and detailed report in TestRunStrategy

--- pysheetgrader/grading/strategy/test.py
+++ pysheetgrader/grading/strategy/test.py
@@ -4,8 +4,8 @@ from pysheetgrader.grading.rubric import GradingRubric
 from pysheetgrader.grading.report import GradingReport
 from pysheetgrader.formula_parser import parse_formula
 from pysheetgrader.formula_parser import encode_cell_reference
+from pysheetgrader.formula_parser import get_excel_formula_lambdas
 from pysheetgrader.document import Document
-from sympy import parse_expr
 
 
 class TestRunStrategy(BaseStrategy):
@@ -19,13 +19,15 @@ class TestRunStrategy(BaseStrategy):
         super().__init__(key_document, sub_document, sheet_name,  grading_rubric)
 
     def grade(self):
+        key_sheet = self.key_document.formula_wb[self.sheet_name]
         sub_sheet = self.sub_document.formula_wb[self.sheet_name]
         cell_coord = self.grading_rubric.cell_coord
 
         report = GradingReport()
         report.max_possible_score = self.grading_rubric.score
 
-        sub_raw_formula = sub_sheet[cell_coord].value
+        # TODO: Rever this back to sub_sheet instead of key_sheet.
+        sub_raw_formula = key_sheet[cell_coord].value
 
         # Test runs
         if not self.report_line_prefix:
@@ -35,9 +37,11 @@ class TestRunStrategy(BaseStrategy):
         for test_case in self.grading_rubric.test_cases:
             result_suffix = "PASS"
             try:
-                if not self.test_run_match(test_case, sub_raw_formula):
+                result_match, result, lower_range, upper_range = self.test_run_match(test_case, sub_raw_formula)
+                if not result_match:
                     all_test_pass = False
-                    result_suffix = "FAIL"
+                    result_suffix = f"FAIL\n{self.report_line_prefix}Expected result between {lower_range} " \
+                                    f"and {upper_range}, got {result} instead"
             except Exception as exc:
                 # TODO: Add more profound exception error message later.
                 all_test_pass = False
@@ -51,17 +55,28 @@ class TestRunStrategy(BaseStrategy):
         return report
 
     def test_run_match(self, test_case: GradingTestCase, sub_raw_formula: str):
+        """
+        Runs the passed test case against the passed formula.
+        Will return tuple of (Boolean, calculated value, expected lower range, expected upper range).
+            The Boolean will inform if the test run matches the expected values or not.
+        :param test_case: GradingTestCase instance.
+        :param sub_raw_formula: String of the raw formula.
+        :return: Tuple of (Boolean, Float, Float, Float).
+        """
 
         raw_inputs = test_case.inputs
         encoded_inputs = {encode_cell_reference(cell_coord): raw_inputs[cell_coord] for cell_coord in raw_inputs}
-        # TODO: Add  option to prevent expansion of cells here
-        encoded_formula = parse_formula(sub_raw_formula)
+        local_dict = get_excel_formula_lambdas()
+        local_dict.update(encoded_inputs)
 
-        result = parse_expr(encoded_formula, local_dict=encoded_inputs)
+        result = parse_formula(sub_raw_formula, local_dict=local_dict)
+        expected_lower_range = test_case.expected_output
+        expected_upper_range = test_case.expected_output
 
         if test_case.output_delta:
-            delta = test_case.output_delta
-            expected_output = test_case.expected_output
-            return (expected_output - delta) <= result <= (expected_output + delta)
-        else:
-            return result == test_case.expected_output
+            expected_lower_range -= test_case.output_delta
+            expected_upper_range += test_case.output_delta
+
+        result_match = expected_lower_range <= result <= expected_upper_range
+
+        return result_match, result, expected_lower_range, expected_upper_range
