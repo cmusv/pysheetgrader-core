35303f8cae4a3f844145a26934e2aa8ecc750ad0
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2020-12-17 23:10:07 -0800

    Remove unused run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# TODO: Add proper installation / environment check steps here.
-source venv/bin/activate
-
-KEY_DOC_PATH=sample_excel_files/type_1_key.xlsx
-SUB_DOC_PATH=sample_excel_files/type_1_sub_right_different_formula_1.xlsx
-SCORE_OUTPUT_PATH=$vocareumGradeFile
-REPORT_OUTPUT_PATH=$vocareumReportFile
-pysheetgrader $KEY_DOC_PATH $SUB_DOC_PATH --score-output $SCORE_OUTPUT_PATH --report-output $REPORT_OUTPUT_PATH --verbose
