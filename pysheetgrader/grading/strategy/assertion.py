47bbcd7d23de4ef77df70f87db4d62eb0d7b088e
Andrew Colello <agcolello@gmail.com>
2023-04-06 08:30:00 -0700

    add docs

--- pysheetgrader/grading/strategy/assertion.py
+++ pysheetgrader/grading/strategy/assertion.py
@@ -6,14 +6,21 @@ REGEX = r"\$[a-zA-Z]\d{1,2}"
 class Assertiontrategy(BaseStrategy):
     """
     Used to grade Assertion rubrics.
+
+    get raw key sheet value
+        for each escaped character:
+            replace with computed result in submission
+    
+    evalute as python expression and return bool
+
     """
     def get_submitted_value(self):
         return str(self.sub_sheet_compute[self.cell_coord].value)
 
-    def check_correct(self, sub_cell_value, key_cell_value, key_coord):
+    def check_correct(self, sub_cell_value, key_cell_value, key_coord) -> bool:
         return key_cell_value
 
-    def get_key_value(self, key_coord):
+    def get_key_value(self, key_coord) -> bool:
         raw_expr = self.key_sheet_raw[key_coord].value
         matches = re.findall(REGEX, raw_expr)
 
